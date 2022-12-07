58b8d39535d03ac3f2fa74704869c87247f6f3a7
Sondre <sbr075@uit.no>
2022-12-07 14:07:03 +0100

    A much better day7

--- day7/sol.py
+++ day7/sol.py
@@ -1,54 +1,24 @@
-def get_dir_size(file):
-    size = 0
-    while (line := file.readline()):
-        if "$" in line:
-            file.seek(x)
-            return size
-
-        cmd = line.split()
-        if cmd[0].isnumeric():
-            size += int(cmd[0])
-        
-        x = file.tell()
-    
-    return size
-
-
-def update_dirs(dirs, dir, size):
-    if dir in dirs:
-        addon = 1
-        while f"{dir}_{addon}" in dirs:
-            addon += 1
-        
-        dir = f"{dir}_{addon}"
-
-    dirs[dir] = size
-
-
-def get_dirs(file, dirs):
-    size = 0
-    while (line := file.readline()):
-        if "ls" in line:
-            size += get_dir_size(file)
-        
-        elif ".." in line:
-            return size
-
-        elif "cd" in line:
-            cmd = line.split()
-            dir_size = get_dirs(file, dirs)
-
-            update_dirs(dirs, cmd[-1], dir_size)
-            size += dir_size
-    
-    return size
-
-
+dirs = []
 with open("day7/input.txt", "r") as file:
-    dirs = {}
-    get_dirs(file, dirs)
+    depth = 0
+    for line in file.readlines():
+        if ".." in line:
+            depth -= 1
+            dirs[-2] += dirs[-1]
+            dirs.insert(0, dirs.pop())
 
-    space_req = dirs["/"] - 40000000
+        elif "cd " in line:
+            depth += 1
+            dirs.append(0)
 
-    print(sum([size for size in dirs.values() if size <= 100000]))
-    print(min([size for size in dirs.values() if size >= space_req]))
+        else:
+            cmd = line.split()
+            if cmd[0].isnumeric():
+                dirs[-1] += int(cmd[0])
+
+    for _ in range(depth-1):
+        dirs[-2] += dirs[-1]
+        dirs.insert(0, dirs.pop())
+
+print(sum([size for size in dirs if size <= 100000]))
+print(min([size for size in dirs if size >= dirs[-1] - 40000000]))
